6037eb3119d0bb0570739feb3f0551e42dac48de
ViceLL <72369834+ViceLL@users.noreply.github.com>
2021-11-29 21:39:30 -0400

    Add files via upload

---
+++ PracticaEnClaseINS368Nov29/PracticaEnClaseINS368Nov29/Program.cs
@@ -0,0 +1,43 @@
+﻿bool doorIsOpen = false;
+bool doorIsClosed = false;
+bool doorIsLocked = false;
+bool doorIsUnlocked = false;
+
+Console.WriteLine("Ingrese que accion desea hacer a la puerta: ");
+
+
+if (doorIsOpen = true)
+{
+
+    
+}
+else
+{
+    Console.WriteLine("Error");
+}
+if (doorIsClosed = true)
+{
+
+}
+else
+{
+    Console.WriteLine("Error");
+}
+if (doorIsLocked = true)
+{
+
+}
+else
+{
+    Console.WriteLine("Error");
+}
+if (doorIsUnlocked = true)
+{
+
+}
+else
+{
+    Console.WriteLine("Error");
+}
+
+
---
+++ PracticaEnClaseINS368Nov29/UnitTestPracticaSemana5INS368/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPracticaSemana5INS368
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
